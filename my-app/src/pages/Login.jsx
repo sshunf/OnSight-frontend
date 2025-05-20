990cb009192db0ed03d39c841dc3cb20975dd312
Shun Fujita <sfujit123456@gmail.com>
2025-05-19 21:54:57 -0500

    fixes

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginScreen from '../components/LoginScreen';
+import AsteriskBackground from '../components/AsteriskBackground';
 
 function Login() {
   return (
-    <div className="login-page">
+    <div className="login-page min-h-screen bg-black">
+      <AsteriskBackground />
       <LoginScreen />
     </div>
   );
