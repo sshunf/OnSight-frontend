a87c97f02bbbc63692b3332c4de7201db23809d3
Shun Fujita <sfujit123456@gmail.com>
2025-06-29 09:07:34 +0900

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-router', 'react-router-dom']
+    include: ['react-router', 'react-router-dom'],
+    base: '/'
   }
 });
 
