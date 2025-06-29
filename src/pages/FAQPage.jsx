c72c064e394475439bf55e3789a20642b432fb2f
Soorya Kuppam <soory@MacBook-Pro-681.local>
2025-06-28 19:41:50 -0500

    pages in footer somewhat implemented, footer navigation fixed, landing page rebuilt

---
+++ src/pages/FAQPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlaceholderContent from '../components/PlaceholderContent';
+
+function FAQPage() {
+  return (
+    <PlaceholderContent 
+      title="Frequently Asked Questions" 
+      message="We are currently compiling a list of frequently asked questions based on feedback from our pilot partners. This section will be updated soon."
+    />
+  );
+}
+
+export default FAQPage; 
