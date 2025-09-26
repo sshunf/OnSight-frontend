42f3d3a1b82ff05401894e9bd82daafb6a9820f8
Soorya Kuppam <soory@macbook-pro-681.ads.northwestern.edu>
2025-09-26 16:57:45 -0500

    feat(bg): replace AsteriskBackground with NightBackground; add starfield CSS and component; update App, Login, FoundersNote pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginScreen from '../components/LoginScreen';
-import AsteriskBackground from '../components/AsteriskBackground';
+// import AsteriskBackground from '../components/AsteriskBackground';
+import NightBackground from '../components/NightBackground';
 
 function Login() {
   return (
     <div className="login-page min-h-screen bg-black">
-      <AsteriskBackground />
+      {/* <AsteriskBackground /> */}
+      <NightBackground />
       <LoginScreen />
     </div>
   );
