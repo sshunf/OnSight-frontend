b09cbd9f42608eb05323fe1415555267bf3a2d0f
Soorya Kuppam <sooryakuppam@Sooryas-MacBook-Pro.local>
2025-11-08 18:42:42 -0600

    Asterisk background swapped to night background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import AsteriskBackground from './AsteriskBackground';
 
 const Layout = ({ children }) => {
   const location = useLocation();
 
-  const showBackground = location.pathname !== '/features';
-
   return (
     <div className="relative min-h-screen flex flex-col">
-      {showBackground && <AsteriskBackground />}
       <Header />
       <main className="flex-grow">
         {children}
