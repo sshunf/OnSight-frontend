6a790bde39308df0022130419785d493a6c9af0e
unknown <angelm.mendoza07@gmail.com>
2025-12-17 13:55:43 -0600

    qr code

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const backendTarget = process.env.VITE_BACKEND_URL || 'http://localhost:3001';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: backendTarget,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   optimizeDeps: {
     include: ['react-router', 'react-router-dom'],
-    base: '/'
-  }
+    base: '/',
+  },
 });
-
